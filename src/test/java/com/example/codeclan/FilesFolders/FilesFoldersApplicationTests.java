3ca7be8734b4b3936bfbaf45548a7d246340fa6e
Emma <emmagray183@gmail.com>
2020-02-11 19:35:43 +0000

    initial commit

---
+++ src/test/java/com/example/codeclan/FilesFolders/FilesFoldersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.FilesFolders;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesFoldersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
