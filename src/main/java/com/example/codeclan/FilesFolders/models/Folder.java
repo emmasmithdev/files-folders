7a08e708af2ef88a192e0737d0d2a73504371d66
Emma <emmagray183@gmail.com>
2020-02-11 19:57:21 +0000

    models created

---
+++ src/main/java/com/example/codeclan/FilesFolders/models/Folder.java
@@ -0,0 +1,72 @@
+package com.example.codeclan.FilesFolders.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "folders")
+public class Folder {
+
+    @Column(name = "title")
+    private String title;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "folder")
+    private List<File> files;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public Folder(String title, User user) {
+        this.title = title;
+        this.files = new ArrayList<>();
+        this.user = user;
+    }
+
+    public Folder() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public List<File> getFiles() {
+        return files;
+    }
+
+    public void setFiles(List<File> files) {
+        this.files = files;
+    }
+
+    public User getUser() {
+        return user;
+    }
+
+    public void setUser(User user) {
+        this.user = user;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void addFile(File file){
+        this.files.add(file);
+    }
+}
---
+++ src/main/java/com/example/codeclan/FilesFolders/models/User.java
@@ -0,0 +1,59 @@
+package com.example.codeclan.FilesFolders.models;
+
+import com.fasterxml.jackson.annotation.JsonBackReference;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "users")
+public class User {
+
+    @Column(name = "name")
+    private String name;
+
+    @JsonBackReference
+    @OneToMany(mappedBy = "user")
+    private List<Folder> folders;
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    public User(String name) {
+        this.name = name;
+        this.folders = new ArrayList<>();
+    }
+
+    public User() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Folder> getFolders() {
+        return folders;
+    }
+
+    public void setFolders(List<Folder> folders) {
+        this.folders = folders;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void addFolder(Folder folder){
+        this.folders.add(folder);
+    }
+}
